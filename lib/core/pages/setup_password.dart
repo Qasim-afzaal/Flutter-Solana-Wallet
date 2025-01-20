3de51321d20cd5043058846b02f5cee4e44d7962
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-21 04:05:56 +0500

    add setup password screen

---
+++ lib/core/pages/setup_password.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class SetupPassword extends StatelessWidget {
+  final String? mnemonic;
+  SetupPassword({super.key, required this.mnemonic});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
