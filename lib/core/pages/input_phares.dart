c39809596545c992527207755204cf7da1f0f56e
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-21 04:05:37 +0500

    add input screen

---
+++ lib/core/pages/input_phares.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class InputPhares extends StatelessWidget {
+  const InputPhares({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
