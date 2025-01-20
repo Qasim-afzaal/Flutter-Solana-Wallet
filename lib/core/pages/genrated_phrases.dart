62111e60b1229bd655bdb3a048c0ccfc02ae6091
Qasim-afzaal <qasim.afzaal432@gmail.com>
2025-01-21 04:05:22 +0500

    add screens

---
+++ lib/core/pages/genrated_phrases.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class GenratedPhrases extends StatelessWidget {
+  const GenratedPhrases({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
